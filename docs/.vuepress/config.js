8ec52b4794d9ce3ab17ad2e6a023b683a368f666
Zakir Hossen <web.zakirbd@gmail.com>
2021-08-27 14:48:16 +0600

    feat: firebase support added

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,81 @@
 module.exports = {
-  locales: {
-    // The key is the path for the locale to be nested under.
-    // As a special case, the default locale can use '/' as its path.
-    '/': {
-      lang: 'en-US',
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator',
-    },
-    '/bn/': {
-      lang: 'bn-BD',
-      title: 'VuePress',
-      description: 'Vue 驱动的静态网站生成器',
-    },
-  },
+//   locales: {
+//     // The key is the path for the locale to be nested under.
+//     // As a special case, the default locale can use '/' as its path.
+//     '/': {
+//       lang: 'en-US',
+//       title: 'VuePress English',
+//       description: 'Vue-powered Static Site Generator',
+//     },
+//     '/bn/': {
+//       lang: 'bn-BD',
+//       title: 'VuePress Bangla',
+//       description: 'Vue 驱动的静态网站生成器',
+//     },
+//   },
+  lang: 'en-US',
+  title: 'VuePres Docs',
   head: [['link', { rel: 'icon', href: 'https://vuejs.org/images/logo.png' }]],
 
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
-    ],
-    locales: {
-      '/': {
-        selectLanguageName: 'English',
-      },
-      '/bn/': {
-        selectLanguageName: 'Bengali',
-      },
-    },
+    docsDir: 'docs',
+    // locales: {
+    //   '/': {
+    //     selectLanguageName: 'English',
+    //   },
+    //   '/bn/': {
+    //     selectLanguageName: 'Bangla',
+    //   },
+    // },
     sidebar: 'auto',
+    navbar: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+        {
+          text: 'Guide',
+          link: '/guide/',
+        },
+        {
+          text: 'Reference',
+          children: [
+            {
+              text: 'VuePress',
+              children: [
+                {
+                  text: 'CLI',
+                  link: '/reference/cli.html',
+                },
+                '/reference/config.md',
+                '/reference/frontmatter.md',
+                '/reference/components.md',
+                '/reference/plugin-api.md',
+                '/reference/theme-api.md',
+                '/reference/client-api.md',
+                '/reference/node-api.md',
+              ],
+            },
+            {
+              text: 'Bundlers',
+              children: [
+                '/reference/bundler/webpack.md',
+                '/reference/bundler/vite.md',
+              ],
+            },
+            {
+              text: 'Default Theme',
+              children: [
+                '/reference/default-theme/config.md',
+                '/reference/default-theme/frontmatter.md',
+                '/reference/default-theme/components.md',
+                '/reference/default-theme/markdown.md',
+                '/reference/default-theme/styles.md',
+              ],
+            },
+          ],
+        },
+    ],
     sidebar: {
       '/guide/': [
         '',
---
+++ docs/module/index.md
@@ -0,0 +1,13 @@
+---
+lang: en-US
+title: Title of this page
+description: Description of this page
+sidebar: auto
+---
+
+# From Module Index Page
+lorem ipus
+
+## awesome man
+lorem
+
